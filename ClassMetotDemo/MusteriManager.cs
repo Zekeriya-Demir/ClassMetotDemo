e02aaaeae553ecb2576c0678f92a61c8373f91fe
Zekeriya Demir <zdemir6006@gmail.com>
2021-01-17 16:28:19 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " "+ musteri.Soyad + " eklendi");
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+musteri.Soyad+" silindi");
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            Console.WriteLine(" Müşteri Listesi");
+            foreach (var musteri in musteriler)
+            {
+                
+                Console.WriteLine(musteri.Ad+" "+musteri.Soyad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ali";
+            musteri1.Soyad = "Demir";
+            musteri1.TcNo = "1234567892";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "Şahin";
+            musteri2.TcNo = "34512345874";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriSil(musteri1);
+
+            Musteri[] musteri = new Musteri[]
+            {
+                musteri1,musteri2
+            };
+
+            musteriManager.MusteriListele(musteri);
+        }
+    }
+}
